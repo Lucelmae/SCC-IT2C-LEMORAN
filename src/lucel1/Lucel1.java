50236739bd6ca2b085d34590bf38ca030e563d17
Escaflowne1995 <mike.escaflowne182@yahoo.com>
2024-09-10 09:11:11 +0800

    first repo

---
+++ src/lucel1/Lucel1.java
@@ -0,0 +1,51 @@
+
+package lucel1;
+
+public class Lucel1 {
+
+   
+    public static void main(String[] args) {
+     
+        
+        
+            new Grade();   
+       
+        
+        /*
+        Grades studentGrades = new Grades();
+        studentGrades.addGrades(12345, "lucel", 2.5, 2.6, 1.5, 1.6);
+        studentGrades.viewGrades();*/
+        
+        
+        /*   Products product1 = new Products();
+        product1.addProducts(100, "Widget ", 10.0, 50, 20);
+        product1.viewProducts();
+        */
+      
+        /*    Salaries employee1 = new Salaries();
+        employee1.setSalaryDetails(101, "John Doe", 20.0, 40.0, 100.0);
+        employee1.displaySalary();
+        */
+        
+        
+        /*
+        Salary app = new Salary();
+        app.processSalaries();
+        */
+        
+        
+        
+        /*   Account accountManager = new Account();
+        accountManager.handleAccounts();*/
+        
+     
+        /*       Accounts account = new Accounts();
+        account.addAccount(101, "John", "Doe", "john.doe@example.com", "johndoe", "Password123!");
+        account.viewAccount();*/
+        
+
+        /*    Product productManager = new Product();
+        productManager.manageProducts();*/
+        
+    }
+}
